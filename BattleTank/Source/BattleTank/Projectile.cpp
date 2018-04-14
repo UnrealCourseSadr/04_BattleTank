9ee1b7b85be17f60c67462650e8dda384a89d2db
DESKTOP-DOVB5QM\Mehdi <s.mehdiprog93@gmail.com>
2018-04-14 17:26:17 +0430

    147 Setup Track Throttles

--- BattleTank/Source/BattleTank/Projectile.cpp
+++ BattleTank/Source/BattleTank/Projectile.cpp
@@ -30,9 +30,6 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed)
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile Fired."), Time)
-
 	ProjectileMovementComponent->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovementComponent->Activate();
 }
---
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s: Throttle: %f"), *Name, Throttle)
+
+	// TODO Clamp actual throttle values so player can't overdrive
+}
