1c2b5c49be02b67d784e9b393aa49a53e9783b3d
DESKTOP-DOVB5QM\Mehdi <s.mehdiprog93@gmail.com>
2018-06-02 20:20:44 +0430

    163 Constructor & Begin Play Timing

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -13,13 +13,14 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	UE_LOG(LogTemp, Warning, TEXT("Finch C++: Constructor executed"))
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	UE_LOG(LogTemp, Warning, TEXT("Finch C++: Begin Play executed"))
 }
 
 // Called to bind functionality to input
